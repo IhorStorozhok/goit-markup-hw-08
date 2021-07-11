6df687509fafe56794472afb208db975c9972030
IhorStorozhok <storozhokigor@gmail.com>
2021-07-11 03:12:01 +0300

    final

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@
         openMenuBtn: document.querySelector('.mob-menu__button-icon'),
         closeMenuBtn: document.querySelector('.mob-menu__button-icon--close'),
         menu: document.querySelector('.mob-menu__nav'),
-        buttom: document.querySelector('.mob-menu__button--close'),
+
     };
 
     refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -11,6 +11,6 @@
 
     function toggleMenu() {
         refs.menu.classList.toggle('backdrop--is-hidden');
-        refs.buttom.classList.toggle('backdrop--is-hidden');
+
     }
 })();
